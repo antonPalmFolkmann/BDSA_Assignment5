308a09e6c5d4df4df6be6ddde5a279e347ea89e0
Nicklas Crawley Jørgensen <turuko44@gmail.com>
2021-10-28 20:46:23 +0200

    Implemented IItem interface in new item classes (#6)
    Refactored UpdateQuality method in Program
    
    Co-authored-by: Nicklas Jørgensen <ncjo@itu.dk>

--- GildedRose.Tests/TestAssemblyTests.cs
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -16,8 +16,8 @@ namespace GildedRose.Tests
         public void Test_Brie_Age_One_Day_Quality_Increases_By_One()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }
+            IList<IItem> items = new List<IItem>{
+                new AgedBrie { Name = "Aged Brie", SellIn = 2, Quality = 0 }
             };
             var app = new Program(){Items = items};
 
@@ -25,16 +25,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
 
             //Then
-            Assert.Equal(1, app.Items[0].Quality);
-            Assert.Equal(1, app.Items[0].SellIn);
+            Assert.Equal(1, app.Items[0].GetQuality());
+            Assert.Equal(1, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Brie_Age_One_Day_Past_SellIn_Date_Quality_Increases_By_Two()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "Aged Brie", SellIn = -1, Quality = 0 }
+            IList<IItem> items = new List<IItem>{
+                new AgedBrie { Name = "Aged Brie", SellIn = -1, Quality = 0 }
             };
             var app = new Program(){Items = items};
 
@@ -42,16 +42,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
 
             //Then
-            Assert.Equal(2, app.Items[0].Quality);
-            Assert.Equal(-2, app.Items[0].SellIn);
+            Assert.Equal(2, app.Items[0].GetQuality());
+            Assert.Equal(-2, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Brie_Age_One_Day_Quality_Stops_At_50()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 }
+            IList<IItem> items = new List<IItem>{
+                new AgedBrie { Name = "Aged Brie", SellIn = 2, Quality = 50 }
             };
             var app = new Program(){Items = items};
 
@@ -59,16 +59,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
 
             //Then
-            Assert.Equal(50, app.Items[0].Quality);
-            Assert.Equal(1, app.Items[0].SellIn);
+            Assert.Equal(50, app.Items[0].GetQuality());
+            Assert.Equal(1, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Vest_Age_One_Day_Quality_Decreases_By_One()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }
+            IList<IItem> items = new List<IItem>{
+                new Normal { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }
             };
             var app = new Program(){Items = items};
 
@@ -76,16 +76,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
         
             //Then
-            Assert.Equal(19, app.Items[0].Quality);
-            Assert.Equal(9, app.Items[0].SellIn);
+            Assert.Equal(19, app.Items[0].GetQuality());
+            Assert.Equal(9, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Vest_Age_One_Day_Past_SellIn_Date_Quality_Decreases_By_Two()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 20 }
+            IList<IItem> items = new List<IItem>{
+                new Normal { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 20 }
             };
             var app = new Program(){Items = items};
 
@@ -93,16 +93,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
         
             //Then
-            Assert.Equal(18, app.Items[0].Quality);
-            Assert.Equal(-2, app.Items[0].SellIn);
+            Assert.Equal(18, app.Items[0].GetQuality());
+            Assert.Equal(-2, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Vest_Age_One_Quality_Stops_At_0()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "+5 Dexterity Vest", SellIn = 2, Quality = 0 }
+            IList<IItem> items = new List<IItem>{
+                new Normal { Name = "+5 Dexterity Vest", SellIn = 2, Quality = 0 }
             };
             var app = new Program(){Items = items};
 
@@ -110,16 +110,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
         
             //Then
-            Assert.Equal(0, app.Items[0].Quality);
-            Assert.Equal(1, app.Items[0].SellIn);
+            Assert.Equal(0, app.Items[0].GetQuality());
+            Assert.Equal(1, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Elixir_Age_One_Day_Decreases_Quality_By_One()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 }
+            IList<IItem> items = new List<IItem>{
+                new Normal { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 }
             };
             var app = new Program(){Items = items};
 
@@ -127,16 +127,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(6, app.Items[0].Quality);
-            Assert.Equal(4, app.Items[0].SellIn);
+            Assert.Equal(6, app.Items[0].GetQuality());
+            Assert.Equal(4, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Sulfuras_Age_One_Day_SellIn_Day_0_Quality_Does_Not_Change()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }
+            IList<IItem> items = new List<IItem>{
+                new Sulfuras { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }
             };
             var app = new Program(){Items = items};
 
@@ -144,16 +144,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(80, app.Items[0].Quality);
-            Assert.Equal(0, app.Items[0].SellIn);
+            Assert.Equal(80, app.Items[0].GetQuality());
+            Assert.Equal(0, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_Sulfuras_Age_One_Day_SellIn_Day_MinusOne_Quality_Does_Not_Change()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 }
+            IList<IItem> items = new List<IItem>{
+                new Sulfuras { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 }
             };
             var app = new Program(){Items = items};
 
@@ -161,16 +161,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(80, app.Items[0].Quality);
-            Assert.Equal(-1, app.Items[0].SellIn);
+            Assert.Equal(80, app.Items[0].GetQuality());
+            Assert.Equal(-1, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_BackstagePass_Age_One_Day_With_Sellin_Date_100_Quality_Increases_By_One()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { 
+            IList<IItem> items = new List<IItem>{
+                new BackStagePass { 
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 100,
                     Quality = 20 }
@@ -181,16 +181,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(21, app.Items[0].Quality);
-            Assert.Equal(99, app.Items[0].SellIn);
+            Assert.Equal(21, app.Items[0].GetQuality());
+            Assert.Equal(99, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_BackstagePass_Age_One_Day_With_Sellin_Date_10_Quality_Increases_By_Two()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { 
+            IList<IItem> items = new List<IItem>{
+                new BackStagePass { 
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 20 }
@@ -201,16 +201,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(22, app.Items[0].Quality);
-            Assert.Equal(9, app.Items[0].SellIn);
+            Assert.Equal(22, app.Items[0].GetQuality());
+            Assert.Equal(9, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_BackstagePass_Age_One_Day_With_Sellin_Date_5_Quality_Increases_By_Three()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { 
+            IList<IItem> items = new List<IItem>{
+                new BackStagePass { 
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 20 }
@@ -221,16 +221,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(23, app.Items[0].Quality);
-            Assert.Equal(4, app.Items[0].SellIn);
+            Assert.Equal(23, app.Items[0].GetQuality());
+            Assert.Equal(4, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Test_BackstagePass_Age_One_Day_With_Sellin_Date_0_Quality_Goes_To_Zero()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { 
+            IList<IItem> items = new List<IItem>{
+                new BackStagePass { 
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 0,
                     Quality = 20 }
@@ -241,16 +241,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(0, app.Items[0].Quality);
-            Assert.Equal(-1, app.Items[0].SellIn);
+            Assert.Equal(0, app.Items[0].GetQuality());
+            Assert.Equal(-1, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Conjured_Items_Aged_One_Day_Quality_Goes_Two_Down_AND_Sellin_Goes_Down_By_One()
         {
             //Arrange
-            IList<Item> items = new List<Item>{
-                new Item { 
+            IList<IItem> items = new List<IItem>{
+                new Conjured { 
                     Name = "Conjured Mana Cake",
                     SellIn = 10,
                     Quality = 20 }
@@ -261,16 +261,16 @@ namespace GildedRose.Tests
             app.UpdateQuality();
 
             //Assert
-            Assert.Equal(18, app.Items[0].Quality);
-            Assert.Equal(9, app.Items[0].SellIn);
+            Assert.Equal(18, app.Items[0].GetQuality());
+            Assert.Equal(9, app.Items[0].GetSellIn());
         }
 
         [Fact]
         public void Conjured_Items_Aged_One_Day_With_Sellin_Date_0_Quality_Goes_With_Four()
         {
             //Given
-            IList<Item> items = new List<Item>{
-                new Item { 
+            IList<IItem> items = new List<IItem>{
+                new Conjured { 
                     Name = "Conjured Mana Cake",
                     SellIn = 0,
                     Quality = 20 }
@@ -281,8 +281,8 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             
             //Then
-            Assert.Equal(16, app.Items[0].Quality);
-            Assert.Equal(-1, app.Items[0].SellIn);
+            Assert.Equal(16, app.Items[0].GetQuality());
+            Assert.Equal(-1, app.Items[0].GetSellIn());
         }
     }
 }
--- GildedRose/Classes/Normal.cs
+++ GildedRose/Classes/Normal.cs
@@ -1,3 +1,20 @@
 namespace GildedRose{
-    
+    public class Normal : Item, IItem
+    {
+        public string GetName() => Name;
+
+        public int GetQuality() => Quality;
+
+        public int GetSellIn() => SellIn;
+        public void UpdateItem()
+        {
+            if (Quality > 0){Quality = Quality - 1;}
+            SellIn--;
+            if (SellIn < 0){
+                if (Quality > 0){
+                    Quality = Quality - 1;
+                }
+            }
+        }
+    }
 }
